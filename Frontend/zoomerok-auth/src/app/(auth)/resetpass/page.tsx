31d5d346189a26bc5e9e4ab475700a142df494cd
PC Raipus <nikita18.907@gmail.com>
2025-04-12 16:21:38 +0300

    Добавлена логика обновления пароля

---
+++ Frontend/zoomerok-auth/src/app/(auth)/resetpass/page.tsx
@@ -0,0 +1,133 @@
+"use client";
+
+import { setCookie } from "cookies-next";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import ErrorNotification from "@/component/ErrorNotification";
+import { useErrorNotification } from "@/hooks/useErrorNotification";
+
+interface IFormStateLogin {
+  email: string;
+}
+
+export default function ResetPassPage() {
+  const { register, handleSubmit } = useForm<IFormStateLogin>();
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  const [finished, setFinished] = useState(false);
+  const { error, showError, showNotification, hideNotification } =
+    useErrorNotification();
+
+  const onSubmit: SubmitHandler<IFormStateLogin> = async (data) => {
+    setLoading(true);
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/reqresetpass`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data),
+        }
+      );
+
+      const data1 = await response.json();
+
+      if (!response.ok) {
+        showNotification(data1.message || "Произошла ошибка");
+        return;
+      } else {
+        setFinished(true);
+      }
+    } catch (error) {
+      showNotification(
+        error instanceof Error ? error.message : "Неизвестная ошибка"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className="bg-cover">
+      <main className="grid justify-items-center">
+        <div className="grid place-items-center h-screen">
+          {loading ? (
+            <button
+              type="button"
+              className="m-[300px] inline-flex items-center"
+            >
+              <svg
+                className="-ml-1 mr-3 h-5 w-5 animate-spin text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              <p className="text-2xl">Загрузка...</p>
+            </button>
+          ) : finished ? (
+            <div className="div">
+              <p>
+                На указанную почту отправлено письмо с ссылкой на восстановление
+                пароля!
+              </p>
+              <div className="mt-3 grid justify-items-center hover:scale-102 duration-300">
+                <Link href="/signin">↩ Вернуться назад</Link>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <ErrorNotification
+                message={error || ""}
+                show={showError}
+                onClose={hideNotification}
+                duration={5000}
+              />
+              <div>
+                <form
+                  onSubmit={handleSubmit(onSubmit)}
+                  className="grid space-y-4 justify-self-center"
+                >
+                  <h1 className="mb-3 justify-self-center text-2xl">
+                    Восстановление пароля
+                  </h1>
+                  <input
+                    className="rounded-md border-[1px] border-black p-1 text-black bg-white"
+                    placeholder="Ваша почта"
+                    type="text"
+                    {...register("email", { required: true })}
+                  />
+                  <button
+                    type="submit"
+                    className="hover:scale-102 rounded-md bg-[#3D8361] px-5 py-2 text-xl text-white duration-300 hover:bg-[#2F6A4E]"
+                  >
+                    Восстановить пароль
+                  </button>
+                </form>
+              </div>
+              <div className="mt-3 grid justify-items-center hover:scale-102 duration-300">
+                <Link href="/signin">↩ Вернуться назад</Link>
+              </div>
+            </div>
+          )}
+        </div>
+      </main>
+    </div>
+  );
+}
