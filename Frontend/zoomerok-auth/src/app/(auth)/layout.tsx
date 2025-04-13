8d791874edbc99da8e8b47f7c3760a083f87e762
PC Raipus <nikita18.907@gmail.com>
2025-04-13 19:04:49 +0300

    Изменена логика layout.tsx аутентификации
    Добавлена проверка на подтверждение почты

--- Frontend/zoomerok-auth/src/app/(auth)/layout.tsx
+++ Frontend/zoomerok-auth/src/app/(auth)/layout.tsx
@@ -2,23 +2,37 @@
 
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
+import { getToken } from "@/utils/auth/getToken";
+import jwt from "jsonwebtoken";
 
-import { getTokens } from "@/utils/auth/getToken";
+interface JwtPayload {
+  ConfirmEmail: boolean;
+}
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const router = useRouter();
   const [hasTokens, setHasTokens] = useState(true);
+  const [confirmEmail, setConfirmEmail] = useState<boolean | null>(null);
 
   useEffect(() => {
     async function TokenCheck() {
-      const accessToken = await getTokens();
+      const accessToken = await getToken();
       if (!accessToken) {
         setHasTokens(false);
       } else {
-        setHasTokens(true);
-        router.push(
-          `${process.env.NEXT_PUBLIC_NETWORK_URL}/callback#token=${accessToken}`
-        );
+        try {
+          const decoded = jwt.decode(accessToken) as JwtPayload;
+          setConfirmEmail(decoded?.ConfirmEmail);
+        } catch (error) {
+          setHasTokens(false);
+        }
+        if (confirmEmail) {
+          setHasTokens(true);
+          router.push(`${process.env.NEXT_PUBLIC_NETWORK_URL}`);
+        } else {
+          setHasTokens(false);
+          router.push("/confirm-email");
+        }
       }
     }
     TokenCheck();
