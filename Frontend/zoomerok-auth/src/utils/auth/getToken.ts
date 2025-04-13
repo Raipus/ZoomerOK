8eb899259a95aa380c351228befe4769641ac001
PC Raipus <nikita18.907@gmail.com>
2025-04-13 19:00:32 +0300

    Переименована функция getTokens в getToken согласно новой стратегии аутентификации

--- Frontend/zoomerok-auth/src/utils/auth/getToken.ts
+++ Frontend/zoomerok-auth/src/utils/auth/getToken.ts
@@ -2,12 +2,10 @@ import { getCookie, hasCookie } from "cookies-next";
 
 type Token = null | undefined | string;
 
-export async function getTokens(): Promise<Token> {
+export async function getToken(): Promise<Token> {
+  let token;
 
-  let token
-
-  if (hasCookie("access_token"))
-    token = getCookie("access_token") as Token;
+  if (hasCookie("access_token")) token = getCookie("access_token") as Token;
 
   if (!token) {
     return null;
