13c1ebb1f555f81ad3755ad9ea519fcdd6f3f39e
PC Raipus <nikita18.907@gmail.com>
2025-04-13 19:03:59 +0300

    Реализована страница-уведомление для людей, не подтвердивших почту

---
+++ Frontend/zoomerok-auth/src/app/(auth)/confirm-email/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default function ConfirmEmailPage() {
+  return (
+    <div className="bg-cover">
+      <main className="grid justify-items-center">
+        <div className="grid place-items-center h-screen">
+          <div className="grid place-items-center">
+            <p>Благодарим за регистрацию!</p>
+            <p>
+              На указанную Вами почту отправлено письмо с ссылкой на её
+              подтверждение.
+            </p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
