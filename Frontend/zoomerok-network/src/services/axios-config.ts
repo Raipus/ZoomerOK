53b252a823f1e414ececfc0990faba03a0d93637
PC Raipus <nikita18.907@gmail.com>
2025-04-23 13:10:56 +0300

    Настроен экземпляр axios для дальнейшей работы с ним
    Введены базовые настройки и добавлены interceptors, автоматически добавляющие токен доступа и проверяющие http response ошибку 401.

---
+++ Frontend/zoomerok-network/src/services/axios-config.ts
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+export const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = Cookies.get('access_token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove('access_token')
+      window.location.replace(`${import.meta.env.VITE_AUTH_URL}/signin`)
+    }
+    return Promise.reject(error)
+  },
+)
