648a51d33f88235faa092ed90b8ac664dc5360d2
igor-izvekov <izveigor+github@gmail.com>
2025-05-07 00:36:38 +0300

    fix: entrypoint.sh запускает backend.sh и frontend docker-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,77 +2,19 @@
 
 set -e # Останавливаем скрипт при первой же ошибке
 
-# Проверяем, существует ли сеть gateway_network
-if ! docker network inspect gateway_network > /dev/null 2>&1; then
-  echo "Создаем сеть gateway_network..."
-  docker network create gateway_network
-  if [ $? -ne 0 ]; then
-    echo "Ошибка при создании сети gateway_network"
-    exit 1
-  fi
-  echo "Сеть gateway_network создана."
-else
-  echo "Сеть gateway_network уже существует."
-fi
+echo "Запускаем Backend..."
 
-# Проверяем, существует ли сеть kafka_network
-if ! docker network inspect kafka_network > /dev/null 2>&1; then
-  echo "Создаем сеть kafka_network..."
-  docker network create kafka_network
-  if [ $? -ne 0 ]; then
-    echo "Ошибка при создании сети kafka_network"
-    exit 1
-  fi
-  echo "Сеть kafka_network создана."
-else
-  echo "Сеть kafka_network уже существует."
-fi
-
-# Проверяем, существует ли volume frontend_static
-if ! docker volume inspect frontend_static > /dev/null 2>&1; then
-  echo "Создаем volume frontend_static..."
-  docker volume create frontend_static
-  if [ $? -ne 0 ]; then
-    echo "Ошибка при создании сети frontend_static"
-    exit 1
-  fi
-  echo "volume frontend_static создана."
-else
-  echo "volume frontend_static уже существует."
-fi
-
-echo "Запускаем Backend/account..."
-docker compose -f Backend/account/docker-compose.yml up -d --build
+# Предполагается, что backend.sh находится в той же директории, что и entrypoint.sh
+# Если нет, укажите правильный путь. Например: ../backend.sh
+cd Backend
+./backend.sh
 if [ $? -ne 0 ]; then
-  echo "Ошибка при запуске Backend/account"
+  echo "Ошибка при запуске Backend"
   exit 1
 fi
-echo "Backend/account запущен."
+echo "Backend запущен."
 
-echo "Запускаем Backend/blog..."
-docker compose -f Backend/blog/docker-compose.yml up -d --build
-if [ $? -ne 0 ]; then
-  echo "Ошибка при запуске Backend/blog"
-  exit 1
-fi
-echo "Backend/blog запущен."
-
-echo "Запускаем Backend/gateway..."
-docker compose -f Backend/gateway/docker-compose.yml up -d --build
-if [ $? -ne 0 ]; then
-  echo "Ошибка при запуске Backend/gateway"
-  exit 1
-fi
-echo "Backend/gateway запущен."
-
-echo "Запускаем Backend/kafka..."
-docker compose -f Backend/docker-compose.kafka.yml up -d --build
-if [ $? -ne 0 ]; then
-  echo "Ошибка при запуске Backend/kafka"
-  exit 1
-fi
-echo "Backend/kafka запущен."
-
+cd ..
 echo "Запускаем Frontend..."
 docker compose -f Frontend/docker-compose.yml up -d --build
 if [ $? -ne 0 ]; then
