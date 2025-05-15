ca4e2d2db4268a41d482f8f2335d431f35f03c1a
igor-izvekov <izveigor+github@gmail.com>
2025-05-15 23:06:30 +0300

    fix: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,19 @@ set -e # Останавливаем скрипт при первой же оши
 echo "Запускаем Backend..."
 
 # Предполагается, что backend.sh находится в той же директории, что и entrypoint.sh
-# Если нет, укажите правильный путь. Например: ../backend.sh
-cd Backend
-./backend.sh
+echo "Запускаем Backend..."
+docker compose -f Backend/docker-compose.yml up -d --build
 if [ $? -ne 0 ]; then
   echo "Ошибка при запуске Backend"
   exit 1
 fi
 echo "Backend запущен."
 
-cd ..
+sleep 1
+
+docker exec backend_kafka /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1 --topic account_blog_topic && \
+docker exec backend_kafka /opt/kafka/bin/kafka-topics.sh --create --bootstrap-server kafka:9092 --partitions 1 --replication-factor 1 --topic blog_account_topic
+
 echo "Запускаем Frontend..."
 docker compose -f Frontend/docker-compose.yml up -d --build
 if [ $? -ne 0 ]; then
